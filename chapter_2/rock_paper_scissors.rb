f46ec4b2e55db5ddc2cb78acb5cda16c3f047284
Katherine Ebel <kathyebel@me.com>
2016-03-23 17:16:09 -0400

    Moved some of main loop logic into separate methods

--- chapter_2/rock_paper_scissors.rb
+++ chapter_2/rock_paper_scissors.rb
@@ -1,4 +1,9 @@
-VALID_CHOICES = { 'r' => 'rock', 'p' => 'paper', 'sc' => 'scissors', 'sp' => 'spock', 'l' => 'lizard' }.freeze
+VALID_CHOICES = {
+  'r' => 'rock',
+  'p' => 'paper',
+  'sc' => 'scissors',
+  'sp' => 'spock',
+  'l' => 'lizard' }.freeze
 
 scores = { player: 0, computer: 0 }
 
@@ -6,20 +11,42 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def win?(player_1, player_2)
-  case player_1
-  when 'scissors' then player_2 == 'paper' || player_2 == 'lizard'
-  when 'paper' then player_2 == 'rock' || player_2 == 'spock'
-  when 'rock' then player_2 == 'lizard' || player_2 == 'scissors'
-  when 'lizard' then player_2 == 'spock' || player_2 == 'paper'
-  when 'spock' then player_2 == 'scissors' || player_2 == 'rock'
+# choice methods
+def set_player_choice
+  choice = ''
+  loop do
+    prompt 'Pick your choice from:'
+    VALID_CHOICES.each { |key, value| puts "\t(#{key}) for #{value}" }
+    choice = gets.chomp
+    break if valid? choice
+    prompt "That's not a valid choice."
   end
+  choice
+end
+
+def valid?(input)
+  VALID_CHOICES.key? input
+end
+
+def set_computer_choice
+  VALID_CHOICES[VALID_CHOICES.keys.sample]
+end
+
+def display_choices(player_1, player_2)
+  puts "You chose: #{VALID_CHOICES[player_1]}, Computer chose: #{player_2}"
 end
 
 def game_over?(scores)
   scores.value? 5
 end
 
+def play_again?
+  prompt 'Do you want to play again?'
+  answer = gets.chomp
+  answer.downcase.start_with? 'y'
+end
+
+# scores methods
 def reset(scores)
   scores.each_key { |key| scores[key] = 0 }
 end
@@ -29,6 +56,21 @@ def update_scores(scores, player, computer)
     scores[:player] += 1
   elsif win?(computer, player)
     scores[:computer] += 1
+  end
+end
+
+def display_scores(scores)
+  prompt "Your score: #{scores[:player]}, Computer score: #{scores[:computer]}"
+end
+
+# results methods
+def win?(player_1, player_2)
+  case player_1
+  when 'scissors' then player_2 == 'paper' || player_2 == 'lizard'
+  when 'paper' then player_2 == 'rock' || player_2 == 'spock'
+  when 'rock' then player_2 == 'lizard' || player_2 == 'scissors'
+  when 'lizard' then player_2 == 'spock' || player_2 == 'paper'
+  when 'spock' then player_2 == 'scissors' || player_2 == 'rock'
   end
 end
 
@@ -42,10 +84,6 @@ def display_result(player, computer)
   end
 end
 
-def valid?(input)
-  VALID_CHOICES.key? input
-end
-
 welcome_message = <<-MSG
   Welcome to Rock, Paper, Scissors, Lizard, Spock!
   Beat the computer 5 times to win!
@@ -53,35 +91,18 @@ MSG
 prompt welcome_message
 # main play loop
 loop do
-  choice = ''
-  # choose selection loop
-  loop do
-    prompt 'Pick your choice from:'
-    VALID_CHOICES.each_pair do |key, value|
-      puts "\t(#{key}) for #{value}"
-    end
-    choice = gets.chomp
+  choice = set_player_choice
+  computer_choice = set_computer_choice
 
-    break if valid? choice
-    prompt "That's not a valid choice."
-  end
-
-  computer_choice = VALID_CHOICES[VALID_CHOICES.keys.sample]
-
-  puts "You chose: #{VALID_CHOICES[choice]}, Computer chose: #{computer_choice}"
-
+  display_choices(choice, computer_choice)
   display_result(VALID_CHOICES[choice], computer_choice)
+
   update_scores(scores, VALID_CHOICES[choice], computer_choice)
-  prompt "Your score: #{scores[:player]}, Computer score: #{scores[:computer]}"
+  display_scores(scores)
 
   if game_over?(scores)
-    prompt 'Do you want to play again?'
-    answer = gets.chomp
-    if answer.downcase.start_with? 'y'
-      reset(scores)
-    else
-      break
-    end
+    reset(scores)
+    break unless play_again?
   end
 end
 
