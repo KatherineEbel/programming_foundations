16c4c7d7474cf9099684fb224088f72b590e473a
Katherine Ebel <kathyebel@me.com>
2016-03-28 09:44:17 -0400

    Slight code cleanup

--- chapter_3/exercise_code.rb
+++ chapter_3/exercise_code.rb
@@ -167,12 +167,12 @@ munsters = {
 
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
-  false if dot_separated_words.size != 4
+  return false if dot_separated_words.size != 4
   while dot_separated_words.size > 0 do
     word = dot_separated_words.pop
-    false if !is_a_number?(word)
+    return false if !is_a_number?(word)
   end
-  return true
+  true
 end
 
 p dot_separated_ip_address? '10.2.3'
