aca8cf84c95865bc594099365e03340c0117e5f6
Katherine Ebel <kathyebel@me.com>
2016-04-20 09:32:58 -0400

    refactored solution

--- challenges/octal/octal.rb
+++ challenges/octal/octal.rb
@@ -4,18 +4,15 @@ require 'pry'
 # to an octal number if valid.
 class Octal
   def initialize(val)
-    @numbers = val.chars.map(&:to_i)
-  end
-
-  def invalid?
-    @numbers.any? { |num| [8, 9].include? num }
+    @numbers = val
   end
 
   def to_decimal
     return 0 if invalid?
-    power = @numbers.size - 1
-    @numbers.each_with_index.inject(0) do |sum, (num, index)|
-      sum + num * 8 ** (power - index)
-    end
+    @numbers.oct
+  end
+
+  def invalid?
+    @numbers.chars.map.any? { |num| [8, 9].include? num.to_i }
   end
 end
